e160571860835fe1b8d13d70902933dc1a7354ca
prateeksonii <prateek_soni@outlook.com>
2022-12-26 02:00:48 +0530

    init add page

--- src/app/dashboard/common/nav/nav.component.ts
+++ src/app/dashboard/common/nav/nav.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss'],
 })
-export class NavComponent {}
+export class NavComponent implements OnInit {
+  isHome = true;
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.isHome = this.router.url === '/dashboard';
+  }
+}
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -5,12 +5,16 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { HomeComponent } from './home/home.component';
 import { NavComponent } from './common/nav/nav.component';
 import { provideIcons } from '@ng-icons/core';
-import { heroPlusCircleSolid } from '@ng-icons/heroicons/solid';
+import {
+  heroPlusCircleSolid,
+  heroArrowLeftCircleSolid,
+} from '@ng-icons/heroicons/solid';
 import { NgIconComponent } from '@ng-icons/core';
+import { AddComponent } from './add/add.component';
 
 @NgModule({
-  declarations: [HomeComponent, NavComponent],
+  declarations: [HomeComponent, NavComponent, AddComponent],
   imports: [CommonModule, DashboardRoutingModule, NgIconComponent],
-  providers: [provideIcons({ heroPlusCircleSolid })],
+  providers: [provideIcons({ heroPlusCircleSolid, heroArrowLeftCircleSolid })],
 })
 export class DashboardModule {}
